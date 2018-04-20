bcd5a40818b8eced2705cd133dfae45335c682f6
Steve <spvallis@gmail.com>
2018-04-20 21:34:21 +0200

    my website project

---
+++ script.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+
+    $('#menu-btn').click(changeNav);
+
+    function changeNav() {
+        $('#links').toggle(2000);
+        $('#menu-btn').toggleClass('turn')
+    }
+
+
+
+
+
+
+
+
+
+})
